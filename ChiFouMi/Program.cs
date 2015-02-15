8597c09159e1451662a75ef4606ffdf270d59700
Julien Pomez <julien.pomez@outlook.com>
2015-02-15 20:17:24 +0100

    Removed regions and commented code

--- ChiFouMi/Program.cs
+++ ChiFouMi/Program.cs
@@ -47,8 +47,6 @@ namespace ChiFouMi
         {
             _a0 = 0;
 
-            
-
             if (args.Any())
             {
                 if (args[_a0].Equals("roxor"))
@@ -57,24 +55,13 @@ namespace ChiFouMi
                 }
             }
 
-            
-
-            #region intro
-
             _str = "exit";
             _strTextIntro = "Veuillez choisir un signe:";
             Console.WriteLine("Bienvenue dans mon chifumi," + " ici c'est un appli de ROXXXXXXXXXXXXXXXOOR!");
             Console.WriteLine("Taper sur la touche entrée pour commencer une partie," + " ou 'exit' pour quitter.");
 
-            #endregion
-
-            #region jeu
-
-            while (!
-                   Initialize())
+            while (!Initialize())
             {
-                #region intro
-
                 Console.WriteLine(_strTextIntro);
                 for (int i = 0, cnt = 0; i < t.Count; i++)
                 {
@@ -83,91 +70,38 @@ namespace ChiFouMi
 
                 _intUs = (char)(Console.ReadLine()[0] - 48);
 
-                #endregion
-
-                #region
-
-                // TODO Code à garder ? Evolution futur ?
-                // var rand = new Random(DateTime.Now.Millisecond);
-                // var int_uv = rand.Next(1, 6);
-
-                // if (roxorMoMode != false && int_uv == 6)
-                // {
-                // Console.WriteLine("Tu es un roxor contre Spock");
-                // Console.WriteLine("Gagne!");
-                // }
-                // else if (int_us == 1 && int_uv == 1)
-                // {
-                // Console.WriteLine("Pierre contre Pierre!");
-                // Console.WriteLine("Egalite!");
-                // }
-                // else if (roxorMoMode != false && int_uv == 2)
-                // {
-                // Console.WriteLine("Tu es un roxor contre Feuille");
-                // Console.WriteLine("Gagne!");
-                // }
-                // else if (int_us == 1 && int_uv == 2)
-                // {
-                // Console.WriteLine("Pierre contre Feuille!");
-                // Console.WriteLine("Perdu!");
-                // }
-
-                #endregion
-
-                #region business logic
-
                 r = new Random(DateTime.Now.Millisecond);
                 _intUv = (char)(r.Next(1, 4).ToString()[0] - 48);
 
-                // TODO : A refactorer
-                // si ciseau et roxor
                 if (roxorMoMode != rmdi && _intUv == 1)
                 {
                     Console.WriteLine("Tu es un roxor contre Pierre");
                     Console.WriteLine("Gagne!");
                 }
                     
-                    // si ciseau et ciseau
                 else if (_intUs == 1 && _intUv == 1)
                 {
-                    // Console.WriteLine("ciseau contre ciseau!");
-                    // Console.WriteLine("Egalite!");
                     Console.WriteLine("Pierre contre Pierre!");
                     Console.WriteLine("Egalite!");
                 }
                     
-                    // si ciseau et roxor
                 else if (roxorMoMode != rmdi && _intUv == 2)
                 {
                     Console.WriteLine("Tu es un roxor contre Feuille");
                     Console.WriteLine("Gagne!");
                 }
                     
-                    // TODO A simplifier
                 else if (_intUs - 1 == _intUv % 2)
                 {
                     Console.WriteLine("Pierre contre Feuille!");
                     Console.WriteLine("Perdu!");
                 }
-                    
-                    // si ciseau et roxor
+                
                 else if (roxorMoMode != rmdi && _intUv == 3)
                 {
                     Console.WriteLine("Tu es un roxor contre Ciseaux");
                     Console.WriteLine("Gagne!");
                 }
-
-
-
-
-
-
-
-
-
-                    // TODO: better syntaxe, it's more compact. Right ? If ok vote on https://framadate.org/dsfsdfgnys7y7wxkiujpfi1z/admin
-                    // Link DEAD!
-                    // https://framadate.org/dsfsdfgnysfdf7wxkiujpfi1z/admin
                 else if (_intUs == 1 && _intUv == 3)
                 {
                     Console.WriteLine("Pierre contre Ciseaux!");
@@ -183,22 +117,16 @@ namespace ChiFouMi
                     Console.WriteLine("Feuille contre Pierre!");
                     Console.WriteLine("Gagne!");
                 }
-                    
-                    // BUG 758996
                 else if (_intUs == 2 && _intUv == 2)
                 {
                     Console.WriteLine("Feuille contre Feuille!");
                     Console.WriteLine("Egalite!");
                 }
-                    
-                    // Evolution, 14522
                 else if (_intUs == 2 && _intUv == 3)
                 {
                     Console.WriteLine("Feuille contre Ciseaux!");
                     Console.WriteLine("Perdu!");
                 }
-                    
-                    // si ciseau et roxor
                 else if (roxorMoMode && _intUv == 3)
                 {
                     Console.WriteLine("Tu es un roxor contre Ciseaux");
@@ -206,23 +134,14 @@ namespace ChiFouMi
                 }
                 else if (_intUs == 3 && _intUv == 1)
                 {
-                    // BUG 985632
                     Console.WriteLine("Ciseaux contre Pierre!");
                     Console.WriteLine("Perdu!");
                 }
-                    
-                    // BUG 12563
                 else if (_intUs == 3 && _intUv % 2 == 0)
                 {
                     Console.WriteLine("Ciseaux contre Feuille!");
                     Console.WriteLine("Gagne!");
                 }
-                    
-                    // if (roxorMoMode != false && int_uv == 6)
-                    // {
-                    // Console.WriteLine("Tu es un roxor contre Spock");
-                    // Console.WriteLine("Gagne!");
-                    // }
                 else if (_intUs == _intUv)
                 {
                     Console.WriteLine("Ciseaux contre Ciseaux!");
@@ -230,7 +149,6 @@ namespace ChiFouMi
                 }
                 else if (_intUs == 3 && _intUv == 4)
                 {
-                    // Evolution, 956322
                     Console.WriteLine("Ciseaux contre Ciseaux!");
                     Console.WriteLine("Egalite!");
                 }
@@ -257,10 +175,6 @@ namespace ChiFouMi
                     Console.WriteLine("Perdu");
                 }
             }
-
-            #endregion
-
-            #endregion
         }
     }
 }
