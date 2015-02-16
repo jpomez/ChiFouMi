2e31fc69ea4980055cdee5a2fc17c09ddb58f22c
Julien Pomez <julien.pomez@outlook.com>
2015-02-17 00:44:39 +0100

    Tests refacto

--- ChiFouMi.Test/RulesEngineTest.cs
+++ ChiFouMi.Test/RulesEngineTest.cs
@@ -8,148 +8,101 @@ namespace ChiFouMi.Test
 {
     public class RulesEngineTest
     {
+        private readonly RulesEngine engine;
+
+        public RulesEngineTest()
+        {
+            this.engine = new RulesEngine();
+        }
+
         [Fact]
         public void WhenRoxorAgainstTheWorldThenRoxorWins()
         {
-            var engine = new RulesEngine();
-
             foreach (var command in Commands.Signs)
             {
                 foreach (var iaCommand in Commands.Signs)
                 {
-                    Check.That(engine.Run(true, command, iaCommand)).IsEqualTo(string.Format("Tu es un roxor contre {0}{1}Gagne!{1}", iaCommand, Environment.NewLine));
+                    Check.That(this.engine.Run(true, command, iaCommand)).IsEqualTo(string.Format("Tu es un roxor contre {0}{1}Gagne!{1}", iaCommand, Environment.NewLine));
                 }
             }
         }
 
         [Fact]
-        public void WhenPlayerAgainstIaThenGameEndsAsExpected()
+        public void WhenSameSignThenEquality()
         {
-            var engine = new RulesEngine();
-
             foreach (var command in Commands.Signs)
             {
-                Check.That(engine.Run(false, command, command)).IsEqualTo(string.Format("{0} contre {0}!{1}Egalite!{1}", command, Environment.NewLine));
+                Check.That(this.engine.Run(false, command, command)).IsEqualTo(string.Format("{0} contre {0}!{1}Egalite!{1}", command, Environment.NewLine));
             }
         }
 
         [Fact]
-        public void WhenPierreAgainstFeuilleThenLose()
+        public void PaperCoversRock()
         {
-            var engine = new RulesEngine();
-            Check.That(engine.Run(false, Sign.Pierre, Sign.Feuille)).IsEqualTo(string.Format("{0} contre {1}!{2}Perdu!{2}", Sign.Pierre, Sign.Feuille, Environment.NewLine));
+            this.CheckRule(Sign.Feuille, Sign.Pierre);
         }
 
         [Fact]
-        public void WhenPierreAgainstCiseauxThenWin()
+        public void ScissorsCutPaper()
         {
-            var engine = new RulesEngine();
-            Check.That(engine.Run(false, Sign.Pierre, Sign.Ciseaux)).IsEqualTo(string.Format("{0} contre {1}!{2}Gagne!{2}", Sign.Pierre, Sign.Ciseaux, Environment.NewLine));
+            this.CheckRule(Sign.Ciseaux, Sign.Feuille);
         }
 
         [Fact]
-        public void WhenFeuilleAgainstPierreThenWin()
+        public void RockCrushesScissors()
         {
-            var engine = new RulesEngine();
-            Check.That(engine.Run(false, Sign.Feuille, Sign.Pierre)).IsEqualTo(string.Format("{0} contre {1}!{2}Gagne!{2}", Sign.Feuille, Sign.Pierre, Environment.NewLine));
-        }
-
-        [Fact]
-        public void WhenFeuilleAgainstCiseauxThenLose()
-        {
-            var engine = new RulesEngine();
-            Check.That(engine.Run(false, Sign.Feuille, Sign.Ciseaux)).IsEqualTo(string.Format("{0} contre {1}!{2}Perdu!{2}", Sign.Feuille, Sign.Ciseaux, Environment.NewLine));
-        }
-
-        [Fact]
-        public void WhenCiseauxAgainstPierreThenLose()
-        {
-            var engine = new RulesEngine();
-            Check.That(engine.Run(false, Sign.Ciseaux, Sign.Pierre)).IsEqualTo(string.Format("{0} contre {1}!{2}Perdu!{2}", Sign.Ciseaux, Sign.Pierre, Environment.NewLine));
-        }
-
-        [Fact]
-        public void WhenCiseauxAgainstFeuilleThenWin()
-        {
-            var engine = new RulesEngine();
-            Check.That(engine.Run(false, Sign.Ciseaux, Sign.Feuille)).IsEqualTo(string.Format("{0} contre {1}!{2}Gagne!{2}", Sign.Ciseaux, Sign.Feuille, Environment.NewLine));
+            this.CheckRule(Sign.Pierre, Sign.Ciseaux);
         }
 
         [Fact]
         public void SpockSmashesScissors()
         {
-            var engine = new RulesEngine();
-            Check.That(engine.Run(false, Sign.Spock, Sign.Ciseaux))
-                .IsEqualTo(string.Format("{0} contre {1}!{2}Gagne!{2}", Sign.Spock, Sign.Ciseaux, Environment.NewLine));
-
-            Check.That(engine.Run(false, Sign.Ciseaux, Sign.Spock))
-                .IsEqualTo(string.Format("{0} contre {1}!{2}Perdu!{2}", Sign.Ciseaux, Sign.Spock, Environment.NewLine));
+            this.CheckRule(Sign.Spock, Sign.Ciseaux);
         }
 
         [Fact]
         public void LizardPoisonsSpock()
         {
-            var engine = new RulesEngine();
-            Check.That(engine.Run(false, Sign.Lezard, Sign.Spock))
-                .IsEqualTo(string.Format("{0} contre {1}!{2}Gagne!{2}", Sign.Lezard, Sign.Spock, Environment.NewLine));
-
-            Check.That(engine.Run(false, Sign.Spock, Sign.Lezard))
-                .IsEqualTo(string.Format("{0} contre {1}!{2}Perdu!{2}", Sign.Spock, Sign.Lezard, Environment.NewLine));
+            this.CheckRule(Sign.Lezard, Sign.Spock);
         }
 
         [Fact]
         public void SpockVaporizesRock()
         {
-            var engine = new RulesEngine();
-            Check.That(engine.Run(false, Sign.Spock, Sign.Pierre))
-                .IsEqualTo(string.Format("{0} contre {1}!{2}Gagne!{2}", Sign.Spock, Sign.Pierre, Environment.NewLine));
-
-            Check.That(engine.Run(false, Sign.Pierre, Sign.Spock))
-                .IsEqualTo(string.Format("{0} contre {1}!{2}Perdu!{2}", Sign.Pierre, Sign.Spock, Environment.NewLine));
+            this.CheckRule(Sign.Spock, Sign.Pierre);
         }
 
         [Fact]
         public void LizardEatsPaper()
         {
-            var engine = new RulesEngine();
-            Check.That(engine.Run(false, Sign.Lezard, Sign.Feuille))
-                .IsEqualTo(string.Format("{0} contre {1}!{2}Gagne!{2}", Sign.Lezard, Sign.Feuille, Environment.NewLine));
-
-            Check.That(engine.Run(false, Sign.Feuille, Sign.Lezard))
-                .IsEqualTo(string.Format("{0} contre {1}!{2}Perdu!{2}", Sign.Feuille, Sign.Lezard, Environment.NewLine));
+            this.CheckRule(Sign.Lezard, Sign.Feuille);
         }
 
         [Fact]
         public void ScissorsDecapitatesLizard()
         {
-            var engine = new RulesEngine();
-            Check.That(engine.Run(false, Sign.Ciseaux, Sign.Lezard))
-                .IsEqualTo(string.Format("{0} contre {1}!{2}Gagne!{2}", Sign.Ciseaux, Sign.Lezard, Environment.NewLine));
-
-            Check.That(engine.Run(false, Sign.Lezard, Sign.Ciseaux))
-                .IsEqualTo(string.Format("{0} contre {1}!{2}Perdu!{2}", Sign.Lezard, Sign.Ciseaux, Environment.NewLine));
+            this.CheckRule(Sign.Ciseaux, Sign.Lezard);
         }
 
         [Fact]
         public void PaperDisprovesSpock()
         {
-            var engine = new RulesEngine();
-            Check.That(engine.Run(false, Sign.Feuille, Sign.Spock))
-                .IsEqualTo(string.Format("{0} contre {1}!{2}Gagne!{2}", Sign.Feuille, Sign.Spock, Environment.NewLine));
-
-            Check.That(engine.Run(false, Sign.Spock, Sign.Feuille))
-                .IsEqualTo(string.Format("{0} contre {1}!{2}Perdu!{2}", Sign.Spock, Sign.Feuille, Environment.NewLine));
+            this.CheckRule(Sign.Feuille, Sign.Spock);
         }
 
         [Fact]
         public void RockCrushesLizard()
         {
-            var engine = new RulesEngine();
-            Check.That(engine.Run(false, Sign.Pierre, Sign.Lezard))
-                .IsEqualTo(string.Format("{0} contre {1}!{2}Gagne!{2}", Sign.Pierre, Sign.Lezard, Environment.NewLine));
+            this.CheckRule(Sign.Pierre, Sign.Lezard);
+        }
 
-            Check.That(engine.Run(false, Sign.Lezard, Sign.Pierre))
-                .IsEqualTo(string.Format("{0} contre {1}!{2}Perdu!{2}", Sign.Lezard, Sign.Pierre, Environment.NewLine));
+        private void CheckRule(Sign strongSign, Sign weakSign)
+        {
+            Check.That(this.engine.Run(false, strongSign, weakSign))
+                .IsEqualTo(string.Format("{0} contre {1}!{2}Gagne!{2}", strongSign, weakSign, Environment.NewLine));
+
+            Check.That(this.engine.Run(false, weakSign, strongSign))
+                .IsEqualTo(string.Format("{0} contre {1}!{2}Perdu!{2}", weakSign, strongSign, Environment.NewLine));
         }
     }
 }
